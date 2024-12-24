5ae66349875c0f0007551f33134e6236f7fb14ca
Hamza Ali <aaly.galy@gmail.com>
2024-12-24 14:25:41 +0500

    blockchain

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-
+import axios from 'axios'
+import { ethers, providers } from 'ethers'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [hashData, setHashData] = useState([])
+  function ProviderEther(){
+      let prov = new ethers.providers.Web3Provider(window.ethereum)
+      console.log(prov)
+      setHashData(prov)
+  }
+  
+  useEffect(() => {
+    ProviderEther()
+  }, [])
+  
   return (
-    dds
     <>
       <Navbar siteName="proofPt."/>
+      <div>
+        <h1>Data from the Blockchain pipeline</h1><br />
+      </div>
     </>
   )
 }
