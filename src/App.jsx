04d5149c685dfe458bf66ed96ef23c1ab17b94f1
Hamza Ali <aaly.galy@gmail.com>
2024-12-24 19:13:57 +0500

    blockchain

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,45 @@ function App() {
   const [hashData, setHashData] = useState([])
   function ProviderEther(){
       let prov = new ethers.providers.Web3Provider(window.ethereum)
+      window.ethereum.request({
+    method: "eth_chainId",
+    params: [],
+  }).then((data)=>{
+    console.log(data);
+    
+  })
       console.log(prov)
       setHashData(prov)
   }
   
   useEffect(() => {
-    ProviderEther()
+    // ProviderEther()
+    if(typeof window.ethereum === 'undefined'){
+      alert("please install metamask!")
+    }else{
+      // alert("metamask is installed")
+      ProviderEther()
+    }
   }, [])
   
   return (
     <>
-      <Navbar siteName="proofPt."/>
-      <div>
-        <h1>Data from the Blockchain pipeline</h1><br />
-      </div>
+    <div className="h-[80vh] relative">
+    <div 
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('./bg-hero.jpg')" }}
+    ></div>
+    <div className="relative">
+        <Navbar siteName="proofPt." />
+        <div className="container mx-auto">
+          <div>
+            <p className='text-white font-medium herotxt text-8xl my-9 mx-3 mb-2'>EMPOWERING TRUST WITH BLOCKCHAIN TECHNOLOGY</p>
+            <p className='herotxtSub text-white ml-6 text-2xl tracking-wide'>Secure. Verify. Simplify.</p>
+          </div>
+        </div>
+    </div>
+</div>
+    
     </>
   )
 }
